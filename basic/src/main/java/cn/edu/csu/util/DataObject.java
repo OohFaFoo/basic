b059ab74d7fe0a027134394f8cda90d9e0603700
lichao <497446588@qq.com>
2013-05-08 15:31:37 +0800

    add XMLUtil

---
+++ basic/src/main/java/cn/edu/csu/util/DataObject.java
@@ -0,0 +1,13 @@
+package cn.edu.csu.util;
+
+import java.io.Serializable;
+import java.util.Hashtable;
+
+public class DataObject extends Hashtable implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3264270079911135944L;
+
+}
